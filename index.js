4eda57a7af3cc4b340c23a02a697936f26173246
Alexis Clash <thespi.alexis@gmail.com>
2023-01-07 22:37:14 -0600

    fist commit to GitHub

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("Your name is Alexis, Your height is 65 inches, your country is United States");
+
+function myInfo( name, height, country) {
+    console.log('Your name is' + name + ' Your height is' + height + ' Your country is' + country);
+}
+
+myInfo (' Alexis', ' 65 inches', ' United States');
